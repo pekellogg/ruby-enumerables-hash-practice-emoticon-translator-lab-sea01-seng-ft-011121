cfd69b7dcc92d962b841a8f0d5e4f08714402750
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 10:05:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,7 +38,7 @@ load_library('lib/emoticons.yml')
 
 def get_english_meaning(file_name, japanese_emoticon)
   emoticon_library = load_library(file_name)
-  emoticon_library.each do |key, value|
+  emoticon_library.map do |key, value|
     if value[:japanese] == japanese_emoticon
         puts key
     elsif value[:japanese] != japanese_emoticon
