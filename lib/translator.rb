12b1965b5e9db3b5796c62f91d1803b6b1a107f9
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 08:38:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,30 +6,30 @@ def load_library(file_name)
   file.map do |emoticon_name_key, value|
     file[emoticon_name_key] = {"english": value[0], "japanese": value[1]}
   end
-  emoticons
+  p emoticons
 end
 
 # load_library('lib/emoticons.yml')
 
-def get_english_meaning(file_name, japanese_emoticon)
-  emoticon_lib = load_library(file_name)
-  emoticon_lib.map do |key, value|
-    if value[:japanese] == japanese_emoticon
-      return key
-    end
-  end
-end
+# def get_english_meaning(file_name, japanese_emoticon)
+#   emoticon_lib = load_library(file_name)
+#   emoticon_lib.map do |key, value|
+#     if value[:japanese] == japanese_emoticon
+#       return key
+#     end
+#   end
+# end
 
 # get_english_meaning('lib/emoticons.yml', "(＾ｖ＾)")
 
 
-def get_japanese_emoticon(file_name, western_emoticon)
-  emoticon_lib = load_library(file_name)
-  emoticon_lib.map do |key, value|
-    if value[:english] == western_emoticon
-      p value[:japanese]
-    end
-  end
-end
+# def get_japanese_emoticon(file_name, western_emoticon)
+#   emoticon_lib = load_library(file_name)
+#   emoticon_lib.map do |key, value|
+#     if value[:english] == western_emoticon
+#       p value[:japanese]
+#     end
+#   end
+# end
 
 # get_japanese_emoticon('lib/emoticons.yml', ":)")
