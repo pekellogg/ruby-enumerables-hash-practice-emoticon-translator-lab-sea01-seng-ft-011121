386911291628a4d1cf33cb774b12f5d586b46212
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 08:18:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(file_name)
+  emoticons = YAML.load_file(file_name)
+  emoticons.map do |key, value|
+    emoticons[key] = {"english": value[0], "japanese": value[1]}
+  end
+  emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+# load_library('lib/emoticons.yml')
+
+def get_english_meaning(file_name, japanese_emoticon)
+  emoticon_lib = load_library(file_name)
+  emoticon_lib.map do |key, value|
+    if value[:japanese] == japanese_emoticon
+      return key
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+# get_english_meaning('lib/emoticons.yml', "(＾ｖ＾)")
+
+
+def get_japanese_emoticon(file_name, western_emoticon)
+  emoticon_lib = load_library(file_name)
+  emoticon_lib.map do |key, value|
+    if value[:english] == western_emoticon
+      p value[:japanese]
+    end
+  end
 end
+
+# get_japanese_emoticon('lib/emoticons.yml', ":)")
