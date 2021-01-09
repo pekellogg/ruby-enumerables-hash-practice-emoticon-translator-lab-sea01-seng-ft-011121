d9a3b9a4975d74758e50615384fb2f1fe9456c10
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 10:06:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -40,9 +40,9 @@ def get_english_meaning(file_name, japanese_emoticon)
   emoticon_library = load_library(file_name)
   emoticon_library.map do |key, value|
     if value[:japanese] == japanese_emoticon
-        puts key
+        return key
     elsif value[:japanese] != japanese_emoticon
-        puts "Sorry, that emoticon was not found"
+        return "Sorry, that emoticon was not found"
     end
   end
 end
