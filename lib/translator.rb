33b3dbedb04e6ac61ec63a28486a06073bbbf755
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 09:25:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,12 +38,11 @@ load_library('lib/emoticons.yml')
 
 def get_english_meaning(file_name, japanese_emoticon)
   emoticon_library = load_library(file_name)
-  emoticon_library.map do {|key, value|
+  emoticon_library.each do |key, value|
     if value{:japanese} == japanese_emoticon
       binding.pry
       return key
     end
-  }
   end
 end
 
